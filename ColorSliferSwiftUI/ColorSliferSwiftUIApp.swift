d0a4f004353d8d3520441a2324f01a97c5d6e951
Ilay Sokolov <sokol8807@gmail.com>
2022-10-22 11:56:07 +0300

    Initial Commit

---
+++ ColorSliferSwiftUI/ColorSliferSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliferSwiftUIApp.swift
+//  ColorSliferSwiftUI
+//
+//  Created by Ilya Sokolov on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliferSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
