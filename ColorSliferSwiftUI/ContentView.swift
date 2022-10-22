5788df219f831f626bc26e4062cfc21ece505353
Ilay Sokolov <sokol8807@gmail.com>
2022-10-22 20:45:08 +0300

    выровнял слайдер

--- ColorSliferSwiftUI/ContentView.swift
+++ ColorSliferSwiftUI/ContentView.swift
@@ -7,17 +7,43 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    @State private var redValue = Double()
+    @State private var greenValue = Double()
+    @State private var blueValue = Double()
+    
+    private var colorSet = CGColor(red: 0.5, green: 0.6, blue: 1, alpha: 1)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            Color(red: redSliderValue / 255, green: greenSliderValue / 255, blue: blueSliderValue / 255)
+                .frame(width: 300, height: 170, alignment: .center)
+                .cornerRadius(20)
+            SliderView(value: $redSliderValue)
+            SliderView(value: $greenSliderValue)
+            SliderView(value: $blueSliderValue)
+            Spacer()
+
+        }.padding()
     }
 }
+
+struct SliderView: View {
+    @Binding var value: Double
+    var body: some View{
+        HStack {
+            Text(lround(value).formatted())
+                .frame(width: 40)
+            Slider(value: $value, in: 0...255, step: 1)
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
